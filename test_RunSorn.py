c51d18176026a82d29bd7f12247e2dc558ade815
Saranraj Nambusubramaniyan <sarannsworld@gmail.com>
2019-03-10 23:42:17 +0100

    Update test_RunSorn.py

--- test_RunSorn.py
+++ test_RunSorn.py
@@ -1,13 +1,12 @@
 import unittest
 from sorn.sorn import RunSorn
 
-
 class TestRunSorn(unittest.TestCase):
-
 
 	def test_runsorn(self):
 		
         	self.assertRaises(Exception, RunSorn(phase='Plasticity', matrices=None,time_steps=5).run_sorn([0.])) 
+
 
 if __name__ == '__main__':
 	unittest.main()
