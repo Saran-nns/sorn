dd7f14336b7306f13dec83077de749628cc97d0c
Saranraj Nambusubramaniyan <sarannsworld@gmail.com>
2019-03-10 17:10:51 +0100

    Add files via upload

---
+++ sorn/build/lib/sorn/exceptions.py
@@ -0,0 +1,8 @@
+class ObjectNotFound(Exception):
+
+	""" An Exception risen when an expected object is not found"""
+
+class InvalidOperation(Exception):
+
+	"""An exception risen when an attempted operation doesn't make any sense"""
+	
