2225520ab17cd8ed6757dc12330f3f38e19b33ce
Saranraj Nambusubramaniyan <sarannsworld@gmail.com>
2019-03-10 23:33:27 +0100

    unitest successful

--- test_RunSorn.py
+++ test_RunSorn.py
@@ -2,18 +2,12 @@ import unittest
 from sorn.sorn import RunSorn
 
 
-class TestRunSorn(unittest.TestRunSorn):
+class TestRunSorn(unittest.TestCase):
 
 
-	def test_runsorn(self, inputs):
-
-
-		try:
-        	RunSorn(phase='Plasticity', matrices=None,time_steps=5).run_sorn(inputs)
-    	
-    	except ExceptionType:
-        	self.fail("RunSorn() raised ExceptionType unexpectedly!")
-
+	def test_runsorn(self):
+		
+        	self.assertRaises(Exception, RunSorn(phase='Plasticity', matrices=None,time_steps=5).run_sorn([0.])) 
 
 if __name__ == '__main__':
 	unittest.main()
