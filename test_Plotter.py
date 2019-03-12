9042dd7bcb1d41ccf7401c645f29bc293402b6f2
Saranraj Nambusubramaniyan <sarannsworld@gmail.com>
2019-03-12 16:14:15 +0100

    kickoff v0.1.5

--- test_Plotter.py
+++ test_Plotter.py
@@ -12,6 +12,10 @@ with open('sample_matrices.pkl','rb') as f:
 
 class TestSorn(unittest.TestCase):
 	
+	def test_runsorn(self):
+		
+        	self.assertRaises(Exception, RunSorn(phase='Plasticity', matrices=None,time_steps=5).run_sorn([0.])) 
+        	
 	def test_plotter(self):
 
 		self.assertRaises(Exception, Plotter.hist_outgoing_conn(weights= matrices_dict['Wee'],bin_size=5, histtype = 'bar',savefig = False))
