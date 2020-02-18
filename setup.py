900ea25e64dac75a08e1359d11c31abb44f7382f
Saran-nns <saran_nns@hotmail.com>
2020-02-18 22:43:32 +0100

    source-whl match #2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name = "sorn",
-    version = "0.2.5",
+    version = "0.2.6",
     author = "Saranraj Nambusubramaniyan",
     author_email = "saran_nns@hotmail.com",
     description ="Self-Organizing Recurrent Neural Networks",
