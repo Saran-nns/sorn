a5cd8a60e2cba076a82b9357c8bd4f200907f5b1
Saran-nns <saran_nns@outlook.com>
2020-02-18 22:33:43 +0100

    add doc pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 import os
 from setuptools import setup
+from os import path
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    return open(os.path.join(os.path.dirname(__file__), fname),encoding='utf-8').read()
 
 setup(
     name = "sorn",
