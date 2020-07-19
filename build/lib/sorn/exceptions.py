62f7296dcf535822d97800008aebf4d30ed3a42d
Saran-nns <saran_nns@outlook.com>
2020-07-19 14:19:24 +0200

    broadcasting transfer func; issue #9

--- build/lib/sorn/exceptions.py
+++
@@ -1,8 +0,0 @@
-class ObjectNotFound(Exception):
-
-	""" An Exception risen when an expected object is not found"""
-
-class InvalidOperation(Exception):
-
-	"""An exception risen when an attempted operation doesn't make any sense"""
-	
