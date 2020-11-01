8c3ff1f55f83caa61745d695426e097fad6784d9
Saran-nns <saran_nns@hotmail.com>
2020-11-01 21:50:18 +0100

    pre release 0.3.1

---
+++ build/lib/sorn/__init__.py
@@ -0,0 +1,7 @@
+from . import sorn
+import logging
+
+__author__ = "Saranraj Nambusubramaniyan"
+__version__ = "0.2.10"
+
+logging.basicConfig(level=logging.INFO)
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name = "sorn",
-    version = "0.2.10",
+    version = "0.3.1",
     author = "Saranraj Nambusubramaniyan",
     author_email = "saran_nns@hotmail.com",
     description ="Self-Organizing Recurrent Neural Networks",
