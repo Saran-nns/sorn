5b2b334979a8876ec06311f519503b2ca71190bc
Saran-nns <saran_nns@outlook.com>
2020-11-16 22:22:16 +0100

    issue #13 resolved

--- sorn/__init__.py
+++ sorn/__init__.py
@@ -1,8 +1,8 @@
-from .sorn import Simulator, Trainer
+from sorn.sorn import Simulator, Trainer
 import logging
-from .utils import *
+from sorn.utils import *
 
 __author__ = "Saranraj Nambusubramaniyan"
-__version__ = "0.3.2"
+__version__ = "0.3.6"
 
 logging.basicConfig(level=logging.INFO)
