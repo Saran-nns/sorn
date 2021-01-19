5c46ebe3a6f94d1b59722da429eb397f494d387f
Saran-nns <saran_nns@outlook.com>
2021-01-19 19:02:06 +0100

    plotter, statistics mod test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+import sorn
 from sphinx.ext.autosummary import Autosummary
 from sphinx.ext.autosummary import get_documenter
 from docutils.parsers.rst import directives
@@ -27,22 +28,52 @@ copyright = "2021, Saranraj Nambusubramaniyan"
 author = "Saranraj Nambusubramaniyan"
 
 # The full version, including alpha/beta/rc tags
-release = "0.3.20"
+release = version = sorn.__version__
 
+autosummary_generate = True
+
+# Sphinx gallery configuration
+from sphinx_gallery.sorting import FileNameSortKey
+
+sphinx_gallery_conf = {
+    "examples_dirs": ["../examples"],
+    #'filename_pattern': '^((?!sgskip).)*$',
+    "gallery_dirs": ["gallery"],
+    "doc_module": ("sorn",),
+    "reference_url": {
+        "numpy": "http://docs.scipy.org/doc/numpy",
+        "geopandas": "https://geopandas.readthedocs.io/en/latest/",
+    },
+    "sphinx_gallery": None,
+    "backreferences_dir": "reference",
+    "within_subsection_order": FileNameSortKey,
+}
+
+# Napoleon settings
+napoleon_google_docstring = True
+
+autodoc_mock_imports = ["sorn"]
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
 extensions = [
-    "sphinx.ext.todo",
-    "sphinx.ext.viewcode",
-    "sphinx.ext.autodoc",
-    "sphinx.ext.mathjax",
-    "sphinx.ext.intersphinx",
+    "IPython.sphinxext.ipython_console_highlighting",
+    "IPython.sphinxext.ipython_directive",
     "sphinx.ext.autosummary",
-    "sphinx_rtd_theme",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.inheritance_diagram",
+    "sphinx.ext.intersphinx",
+    "sphinx_gallery.gen_gallery",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -79,9 +110,52 @@ html_theme_options = {"body_max_width": "auto"}
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+# Tell sphinx what the primary language being documented is.
+primary_domain = "py"
+
+# Tell sphinx what the pygments highlight language should be.
+highlight_language = "py"
 
 # def setup(app):
 #     app.add_css_file("_theme.css")
 
 autodoc_default_flags = ["members", "undoc-members", "special-members"]
+autodoc_member_order = "bysource"
+autodoc_mock_imports = ["rpy2"]
 
+
+def setup(app):
+    """
+    Enable documenting 'special methods' using the autodoc_ extension.
+    :param app: The Sphinx application object.
+    This function connects the :func:`special_methods_callback()` function to
+    ``autodoc-skip-member`` events.
+    .. _autodoc: http://www.sphinx-doc.org/en/stable/ext/autodoc.html
+    """
+    app.connect("autodoc-skip-member", special_methods_callback)
+
+
+def special_methods_callback(app, what, name, obj, skip, options):
+    """
+    Enable documenting 'special methods' using the autodoc_ extension.
+    Refer to :func:`enable_special_methods()` to enable the use of this
+    function (you probably don't want to call
+    :func:`special_methods_callback()` directly).
+    This function implements a callback for ``autodoc-skip-member`` events to
+    include documented 'special methods' (method names with two leading and two
+    trailing underscores) in your documentation. The result is similar to the
+    use of the ``special-members`` flag with one big difference: Special
+    methods are included but other types of members are ignored. This means
+    that attributes like ``__weakref__`` will always be ignored (this was my
+    main annoyance with the ``special-members`` flag).
+    The parameters expected by this function are those defined for Sphinx event
+    callback functions (i.e. I'm not going to document them here :-).
+    """
+    import types
+
+    if getattr(obj, "__doc__", None) and isinstance(
+        obj, (types.FunctionType, types.MethodType)
+    ):
+        return False
+    else:
+        return skip
