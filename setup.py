d58ddcfd7486e7862e28b060f6c794e7cd4fb863
Saran-nns <saran_nns@outlook.com>
2021-08-15 05:13:51 +0200

    v0.5.7 update

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name="sorn",
-    version="0.5.6",
+    version="0.5.7",
     author="Saranraj Nambusubramaniyan",
     author_email="saran_nns@hotmail.com",
     description="Self-Organizing Recurrent Neural Networks",
