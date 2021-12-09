09fd16b45d7b81d06e2e3292ca7cbbe1297d3ead
saran_nns <saran_nns@hotmail.com>
2021-12-09 10:35:13 +0100

    remove binary dist

--- build/lib/sorn/__init__.py
+++
@@ -1,8 +0,0 @@
-from .sorn import Simulator, Trainer
-import logging
-from .utils import *
-
-__author__ = "Saranraj Nambusubramaniyan"
-__version__ = "0.6.2"
-
-logging.basicConfig(level=logging.INFO)
